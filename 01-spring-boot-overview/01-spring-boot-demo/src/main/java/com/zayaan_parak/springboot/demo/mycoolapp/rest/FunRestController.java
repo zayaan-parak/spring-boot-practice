e53c2560250093898247978ef3e5f01c66c5982b
Zayaan Parak <zayaan.parak@altron.com>
2024-09-11 16:03:37 +0200

    Initial commit

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/zayaan_parak/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.zayaan_parak.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    // expose "/" that returns Hello World
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#Spring Boot Practice
+
+Done by Zayaan Parak.
